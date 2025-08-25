4a48e94f1f7cf1ee637befb336ab8b25d7baef38
Prichard Dube <genarodube8@gmail.com>
2025-08-25 12:05:28 +0200

    debug: wire up DRF routers, JWT auth endpoints, and today’s activity path

--- fitness_tracker/api/urls.py
+++ fitness_tracker/api/urls.py
@@ -1,6 +1,7 @@
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import UserViewSet, ActivityViewSet, TodayActivityList
-from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet)
@@ -9,5 +10,8 @@ router.register(r'activities', ActivityViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('activities/today/', TodayActivityList.as_view(), name='today-activities'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
+
 
